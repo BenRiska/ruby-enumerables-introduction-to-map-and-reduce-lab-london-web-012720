df786317ce0402690434bbcdfc3cb4653fcd3e55
Ben Taylor <ben10taylor10@hotmail.com>
2020-01-15 16:06:01 +0000

    Automatically backed up by Learn

--- spec/map_and_reduce_spec.rb
+++ spec/map_and_reduce_spec.rb
@@ -6,7 +6,7 @@ describe 'my own map-like methods' do
       expect(map_to_negativize([1, 2, 3, -9])).to eq([-1, -2, -3, 9])
     end
   end
-
+end
   describe "map_to_no_change returns an array with the original values" do
     it "transforms correctly" do
       dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
