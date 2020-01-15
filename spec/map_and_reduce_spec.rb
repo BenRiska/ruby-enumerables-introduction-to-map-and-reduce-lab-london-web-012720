545ea104377c535feddad98ff338a34be586f43c
Ben Taylor <ben10taylor10@hotmail.com>
2020-01-15 16:04:51 +0000

    Automatically backed up by Learn

--- spec/map_and_reduce_spec.rb
+++ spec/map_and_reduce_spec.rb
@@ -5,7 +5,7 @@ describe 'my own map-like methods' do
     it "transforms correctly" do
       expect(map_to_negativize([1, 2, 3, -9])).to eq([-1, -2, -3, 9])
     end
-  end
+  
 
   describe "map_to_no_change returns an array with the original values" do
     it "transforms correctly" do
